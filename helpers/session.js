7c732782a503a3abad31f9204c979a30eea9f91b
German <gerkra92@gmail.com>
2015-11-21 22:34:54 +0300

    session

---
+++ helpers/session.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+    var Session = {};
+
+    Session.register = function (req, res, next) {
+
+    };
+
+    Session.kill = function (req, res, next) {
+
+    };
+
+    Session.isAuthorized = function (req, res, next) {
+
+    };
+
+    Session.isAuthenticated = function (req, res, next) {
+
+    };
+
+    return Session;
+};
